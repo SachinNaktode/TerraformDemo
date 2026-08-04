d537d074876903eaa07cc3c77452f77b0d24f347
Sachin Naktode <sachin.naktode.ext@constl.com>
2026-08-04 21:35:25 +0530

    updated one1

--- Main.tf
+++ Main.tf
@@ -1,10 +1,11 @@
-resource "azurerm_resource_group" "rg1" {
+resource "azurerm_resource_group" "rg" {
 
   name = var.resource_group_name
 
   location = var.location
 
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
 
